8b140d83eb8798d7659b608ffa8be33cc83ac572
2372 Home <2372home@2372s-imac.attlocal.net>
2020-01-21 21:33:24 -0800

    read a specific value

--- Sequelize-Passport-Example/test.js
+++ Sequelize-Passport-Example/test.js
@@ -9,16 +9,28 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT * FROM Users", function (err, result, fields) {
+  con.query("SELECT * FROM Users", function (err, resultUsers, fields) {
     if (err) throw err;
-    console.log(result);
+    //console.log(resultUsers);
+    console.log(resultUsers);
+    console.log(resultUsers[0].email);
+    //console.log(JSON.parse(resultUsers).email);
   });
 
-  con.query("SELECT * FROM Posts", function (err, result, fields) {
+  con.query("SELECT * FROM Posts", function (err, resultTitles, fields) {
     if (err) throw err;
-    console.log(result);
+    console.log(resultTitles);
+    console.log(resultTitles[0].title);
   });
 
 });
 
+//console.log(resultUsers);
+//console.log(resultTitles);
+
+//if (resultTitles === resultUsers) {
+//  console.log("allowed");
+//} else {
+//  console.log("not allowed");
+// }
 
